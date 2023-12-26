5c4f8514f6c43c0ce566287aafacd8dd3dced91f
Prashant Bhat <prashantbhat84@gmail.com>
2023-12-26 21:45:35 +0530

    example user input

--- hello.go
+++ hello.go
@@ -1,9 +1,14 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	fmt.Println("Hello from golang")
+	// user input eg.
+	// fmt.Println("Hello from golang")
+	// reader := bufio.NewReader(os.Stdin)
+	// name, err := reader.ReadString('\n')
+	// if err == nil {
+	// 	fmt.Println("No Error")
+	// 	fmt.Println(name)
+	// } else {
+	// 	log.Fatal(err)
+	// }
 }
